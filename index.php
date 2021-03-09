8c06eb2c17efd3f6ca0a546ce3883a89a6a3574f
Octavio de la Vega <h.o.delavega@gmail.com>
2021-03-09 19:30:01 -0300

    First step

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Ingreso</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-6">
+			<h1>Mostrador de Documentos</h1>
+			<table>
+			</table>
+			<?php 
+				$directory='./files/';
+				$files=scandir($directory);
+				for ($i=0; $i < count($files); $i++){
+					if(strchr($files[$i], ".") and strlen($files[$i])>2){
+						echo "<a href='./files/".$files[$i]."' target='_blanck'>".strchr($files[$i], ".", true)."</a>";
+					}
+				}
+			?>
+		</div>
+		<div class="col-md-3"></div>
+	</div>
+</body>
+</html>
